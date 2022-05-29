f34af1283b0232a12eed34ff908fa3b16922d008
Richard Caleb Lawver <clawver30@gmail.com>
2022-05-29 10:37:08 -0600

    Added api routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,11 +4,11 @@ const router = require('express').Router();
 
 
 router.get('/notes', function(req, res) {
-    response.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
 });
 
 router.get('*', function(req, res) {
-    response.sendFile(path.join(__dirname, '../Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
 });
 
 module.exports = router;
