4557bc57efb7d0599ae2b62461d63c63f133e6d5
Richard Caleb Lawver <clawver30@gmail.com>
2022-06-19 20:29:02 -0600

    cleaned up formatting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-
 const storage = require('../db/storage');
 
 router.get('/notes', (req, res) => {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,5 @@
 const path = require('path');
-
 const router = require('express').Router();
-
 
 router.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
