e432eaf7738699cd3065a2e2b17c69568e72bad6
Richard Caleb Lawver <clawver30@gmail.com>
2022-06-19 20:39:41 -0600

    adding notes to storage.js

--- db/storage.js
+++ db/storage.js
@@ -5,7 +5,7 @@ const util = require("util"); // these are requirements to complete what is done
 const writeFile = util.promisify(fs.writeFile); // writes and reads the file
 const readFile = util.promisify(fs.readFile);
 
-class Storage { // setting up the storage object
+class Storage { // setting up the storage object, functions inside this object will apply to the object
     read() {
         return readFile("db/db.json", "utf8")
     }
@@ -13,28 +13,29 @@ class Storage { // setting up the storage object
         return writeFile("db/db.json", JSON.stringify(note))
     }
 
-    addNote(note) {
-        const { title, text } = note
+    addNote(note) { // adding new notes
+        const { title, text } = note // setting up items to change within the note
 
         if (!title || !text) {
             throw new Error("title and text cannot be blank")
         }
 
-        const newNote = { title, text, id: uuid() }
+        const newNote = { title, text, id: uuid() } // gives the newNote a unique id
 
-        return this.getNotes()
+        return this.getNotes() // will update the notes side bar
             .then(notes => [...notes, newNote])
             .then(updatedNotes => this.write(updatedNotes))
             .then(() => this.newNote)
     }
 
-    getNotes() {
+    getNotes() { // shows current notes
         return this.read()
             .then(notes => {
                 return JSON.parse(notes) || [];
             })
     }
-    removeNote(id) {
+    
+    removeNote(id) { // deletes a note 
         return this.getNotes()
             .then(notes => notes.filter(note => note.id !== id))
             .then(keptNotes => this.write(keptNotes))
